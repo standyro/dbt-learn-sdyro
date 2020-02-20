31ca731ba9ab59e39e904e6875a5ca0feeffcb26
Stan Dyro <stan.dyro@latimes.com>
2020-02-20 19:59:14 +0000

    add stripe payments

--- models/example/customers.sql
+++ models/example/customers.sql
@@ -4,13 +4,23 @@ with customers as (
 orders as (
     select * from {{ ref('stg_orders') }}
 ),
+payments as (
+    select
+        "orderID" as order_id,
+        "paymentMethod" as payment_method,
+        "AMOUNT" as amount,
+        "CREATED" as created
+    from raw.stripe.payment
+),
 customer_orders as (
     select
         customer_id,
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(payments.order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
+    left join payments
     group by 1
 ),
 final as (
@@ -20,6 +30,7 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
+        customer_orders.lifetime_value,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
     from customers
     left join customer_orders using (customer_id)
